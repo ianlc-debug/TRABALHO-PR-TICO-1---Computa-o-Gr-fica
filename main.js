9c5f72944d8b594bf0bd59e7bb054e99be0a5880
Davi Guedes <daviguedes@alu.ufc.br>
2026-05-05 23:31:26 -0300

    Colocando sombra, chão, a posição do tenis e comentando um pouco o código

--- main.js
+++ main.js
@@ -5,37 +5,68 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 let scene, camera, renderer, currentShoe, mainLight, controls;
 
 function init() {
+    //Mundo 3D: scene = new THREE.Scene();
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xf0f0f0);
 
+    //Ponto de visão do usuário(Câmera):
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(0, 0.5, 3);
 
+    //Renderizador WebGL (Desenha tudo usando a GPU)
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
     document.getElementById('canvas-container').appendChild(renderer.domElement);
 
+    //Permite ao usuário interagir com a cena (girar, zoom, movimentar a câmera)
     controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
 
     mainLight = new THREE.DirectionalLight(0xffffff, 2);
     mainLight.position.set(5, 5, 5);
+    mainLight.castShadow = true;
+    mainLight.shadow.mapSize.width = 2048;
+    mainLight.shadow.mapSize.height = 2048;
     scene.add(mainLight);
     scene.add(new THREE.AmbientLight(0xffffff, 0.7));
+
+    //Adicionando o chão:
+    const floor = new THREE.Mesh(
+        new THREE.PlaneGeometry(5, 5),
+        new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.8 })
+    );
+    floor.rotation.x = -Math.PI / 2;
+    floor.position.y = -0.8;
+    floor.receiveShadow = true;
+    scene.add(floor);
 
     const loader = new GLTFLoader();
 
     window.loadShoe = (fileName) => {
         if (currentShoe) scene.remove(currentShoe);
 
+        //Geometria complexa importada externamente (modelo 3D do tênis):
         loader.load(`models/${fileName}.glb`, (gltf) => {
             currentShoe = gltf.scene;
-            
+
+            //Fazendo o tenis gerar sombras:
+            currentShoe.traverse((child) => {
+                if (child.isMesh) {
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                }
+            });
+
             const box = new THREE.Box3().setFromObject(currentShoe);
             const center = box.getCenter(new THREE.Vector3());
-            currentShoe.position.sub(center); 
-            
+            currentShoe.position.sub(center);
+            //Ajustando a posição do tênis para que fique no chão:
+            currentShoe.position.y = -0.35;
+
             scene.add(currentShoe);
         });
     };
