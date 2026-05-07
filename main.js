9b8171aecbc9580d4f10108b7f3832c22e86cc82
Augusto <zonixgamer3609@gmail.com>
2026-05-07 18:30:01 -0300

    Adiciona sistema de pintura por partes, fundo HDRI e texturas do cilindro

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-
+// iluminacao HDRI 
+import { EXRLoader } from 'three/addons/loaders/EXRLoader.js';
 let scene, camera, renderer, currentShoe, mainLight, controls;
 
 function init() {
     //Mundo 3D: scene = new THREE.Scene();
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xf0f0f0);
+
+    // Carregando o ficheiro HDRI (.exr)
+    new EXRLoader().load('models/ferndale_studio_04_4k.exr', (texture) => {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = texture;
+    scene.environment = texture;
+    });
 
     //Ponto de visão do usuário(Câmera):
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 0.5, 3);
+    camera.position.set(8, 3, -8);
 
     //Renderizador WebGL (Desenha tudo usando a GPU)
     renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -19,6 +26,9 @@ function init() {
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    //textura do fundo
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 1.0;
 
     document.getElementById('canvas-container').appendChild(renderer.domElement);
 
@@ -35,11 +45,35 @@ function init() {
     scene.add(new THREE.AmbientLight(0xffffff, 0.7));
 
     //Adicionando o chão:
+    //alterar a geometria ou adicionar uma nova, apagar tambem floor.rotation.x = -Math.PI / 2;
     const floor = new THREE.Mesh(
-        new THREE.PlaneGeometry(5, 5),
+        new THREE.CylinderGeometry(6, 6, 0.2, 64),
         new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.8 })
     );
-    floor.rotation.x = -Math.PI / 2;
+
+    //adicionando textura ao chao
+    // 1. Criar o carregador de texturas
+    const textureLoader = new THREE.TextureLoader();
+
+    // 2. Carregar as imagens
+    const baseColorMap = textureLoader.load('texturas/prata/Collection_VidroReflectaPrata_sl_basecolor.jpg');
+    const metallicMap = textureLoader.load('texturas/prata/Collection_VidroReflectaPrata_sl_metallic.jpg');
+    const normalMap = textureLoader.load('texturas/prata/Collection_VidroReflectaPrata_sl_normal.jpg');
+    const roughnessMap = textureLoader.load('texturas/prata/Collection_VidroReflectaPrata_sl_roughness.jpg');
+    const opacityMap = textureLoader.load('texturas/prata/Collection_VidroReflectaPrata_sl_opacity.jpg');
+
+    // 3. Aplicar o novo material ao cilindro existente
+    floor.material = new THREE.MeshStandardMaterial({ 
+    map: baseColorMap,
+    metalnessMap: metallicMap,
+    roughnessMap: roughnessMap,
+    normalMap: normalMap,
+    metalness: 1.0
+    });
+
+    // 4. Avisar ao Three.js para atualizar a renderização dessa peça
+    floor.material.needsUpdate = true;
+    
     floor.position.y = -0.8;
     floor.receiveShadow = true;
     scene.add(floor);
@@ -48,26 +82,31 @@ function init() {
 
     window.loadShoe = (fileName) => {
         if (currentShoe) scene.remove(currentShoe);
-
+        
         //Geometria complexa importada externamente (modelo 3D do tênis):
         loader.load(`models/${fileName}.glb`, (gltf) => {
             currentShoe = gltf.scene;
 
+            //Ajustar a cor por parte depois
             //Fazendo o tenis gerar sombras:
+            //imprimi no crtl+shift+i (console do navegador) os nomes das peças do tênis para facilitar a identificação e pintura depois
             currentShoe.traverse((child) => {
-                if (child.isMesh) {
-                    child.castShadow = true;
-                    child.receiveShadow = true;
+            if (child.isMesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+                // Esta linha imprime os nomes "Object_2", "Object_3", etc. no console (F12)
+                console.log("Peça encontrada:", child.name);
                 }
             });
-
+            
             const box = new THREE.Box3().setFromObject(currentShoe);
             const center = box.getCenter(new THREE.Vector3());
             currentShoe.position.sub(center);
             //Ajustando a posição do tênis para que fique no chão:
-            currentShoe.position.y = -0.35;
+            currentShoe.position.y = 0.64;
+            currentShoe.scale.set(3, 3, 3);
             if (fileName == 'adidas_ozelia') {
-                currentShoe.scale.set(0.5, 0.5, 0.5);
+                currentShoe.scale.set(1.5, 1.5, 1.5);
                 currentShoe.position.y = -0.79;
                 currentShoe.rotation.y -= 5;
             }
@@ -86,6 +125,28 @@ function animate() {
     controls.update();
     renderer.render(scene, camera);
 }
+
+//seletor de cor para cada parte do tênis
+document.querySelectorAll('.color-part-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        const color = btn.dataset.color;
+        const selectedPart = document.getElementById('partSelector').value;
+
+        if (currentShoe) {
+            currentShoe.traverse((child) => {
+                if (child.isMesh) {
+                    // Verifica se deve pintar tudo ou apenas a peça selecionada no menu
+                    if (selectedPart === "TUDO" || child.name === selectedPart) {
+                        
+                        // Importante: Clonar o material para não pintar outras peças iguais por erro
+                        child.material = child.material.clone();
+                        child.material.color.set(color);
+                    }
+                }
+            });
+        }
+    });
+});
 
 document.getElementById('lightIntensity').addEventListener('input', (e) => {
     mainLight.intensity = parseFloat(e.target.value);
